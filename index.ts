842748c55b2759e0ec3e748039d2e0b5dba3f8fe
MUAZAMALI-dev <malikmuazambhutta@gmail.com>
2023-07-30 13:50:24 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,49 @@
+var myname="Muazam Ali";
+
+console.log(myname);
+let numb1=39;
+let numb2=25;
+let total;
+total=numb1+numb2;
+
+console.log(total);
+
+var firstassignment:string="hey abu hurairah iam learning typescript";
+
+console.log(firstassignment);
+
+let numb3=10;
+let numb4=20;
+let numb5=30;
+let numb6=40;
+let numb7=50;
+let numb8=60;
+let numb9=70;
+let numb10=80;
+let sum;
+sum=numb1+numb2+numb3+numb4+numb5;
+console.log("sum of 5 numbers");
+console.log("sum:"+ sum);
+
+let subtraction;
+subtraction=numb10-numb9-numb8;
+console.log("subtraction of 5 numbers");
+console.log("subtraction:" + subtraction);
+
+let multiplication;
+multiplication=numb1+numb2+numb3+numb4+numb5;
+console.log("Multiplication of 5 numbers");
+console.log("multiplication:" + multiplication);
+
+let divide;
+divide=numb1/numb2/numb3/numb4
+console.log("division of 5 numbers");
+console.log("divide:" + divide);
+
+
+
+let bool;
+bool;let x=89;let y=89;
+bool=!(x==y);
+console.log("boolian result");
+console.log("bool:" + bool);
